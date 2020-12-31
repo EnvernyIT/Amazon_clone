3542c1b39d8e72d48c245b941fba424ebfa8ed0f
EnvernyIT <envernyiengibe@gmail.com>
2020-12-31 07:35:32 -0300

    Created React App

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    //BEM
+    <div className="App">
+     <h1>Hello clever Programmers, let's build the amazon store 🚀 </h1>
+     {/* Header */}
+     {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCcCiudLqAaekaTLdV_9zlO0Fkb2fe3GLM",
+    authDomain: "fir-5ac9b.firebaseapp.com",
+    projectId: "fir-5ac9b",
+    storageBucket: "fir-5ac9b.appspot.com",
+    messagingSenderId: "696701912010",
+    appId: "1:696701912010:web:f957712f33a9a109cb0533",
+    measurementId: "G-5SYTTMNTRN"
+  };
